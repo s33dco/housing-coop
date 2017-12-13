ff7bfea1b55b064e234d0aa8175a4ab1ce10fd7c
s33dco <code@s33d.co>
2017-12-13 00:34:31 +0000

    Property Model, validation & spec

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -1,9 +1,18 @@
 def person_attributes(overrides = {})
   {
-    firstname: "Peter",
-    lastname: "Perfect",
-    email: "example@example.com",
-    phone: "07777777777"
+    firstname:    "Peter",
+    lastname:     "Perfect",
+    email:        "example@example.com",
+    phone:        "07777777777"
   }.merge(overrides)
 end
 
+def property_attributes(overrides = {})
+  {
+    house_name_no: '1',
+    address1:      "Electric Avenue",
+    address2:       "Citytown",
+    postcode:       "BN1 7HZ",
+    rent_per_week:  100.00
+  }.merge(overrides)
+end
