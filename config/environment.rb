dabc624f94aca1035af0b23077ea64c9933ff8b5
s33dco <code@s33d.co>
2018-03-02 00:53:21 +0000

    add dummy static page text, basic template complete

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
 
-ENV['COOP_FULL_NAME']   = 'Dryad Housing Cooperative Ltd'
-ENV['COOP_NAME']				=	'Dryad Housing Coop'
-ENV['COOP_SHORT_NAME']	=	'Dryad'
-ENV['COOP_FOOTER']    	= 'A fully mutual cooperative registered with the financial services authority under the industrial & provident societies act, 1965. Registration number: 28719R'
-ENV['COOP_ADDRESS']			=	'43, Golf Drive, BN1 7HZ'
+ENV['COOP_FULL_NAME']   = 'Fictional Housing Cooperative Ltd'
+ENV['COOP_NAME']				=	'Fictional Housing Coop'
+ENV['COOP_SHORT_NAME']	=	'Fictional'
+ENV['COOP_FOOTER']    	= 'A fully mutual cooperative registered with the financial services authority under the industrial & provident societies act, 1965. Registration number: XXXXXX'
+ENV['COOP_ADDRESS']			=	'Fictional House, Fiction Street, ZZ1 1ZZ'
 
 # Load heroku vars from local file
 heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
 	root "static_pages#welcome"
 
-	get  "static_pages/welcome" => "static_pages#welcome"
-	get  "static_pages/allocations" => "static_pages#allocations"
-	get  "static_pages/contact" => "static_pages#contact"
+	get  "static_pages/welcome" => "static_pages#welcome", as: 'welcome'
+	get  "static_pages/allocations" => "static_pages#allocations", as: 'allocations'
+	get  "static_pages/contact" => "static_pages#contact", as: 'contact'
 
 
   resources :properties
