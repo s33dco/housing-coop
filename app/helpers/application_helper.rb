2a90f8ba0879171ee376c1a485753ca800ed610d
s33dco <code@s33d.co>
2018-03-02 12:47:32 +0000

    happy with basic template and meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,13 @@
 module ApplicationHelper
-	
-	def page_title
-  	if content_for?(:title)
-  		"#{content_for(:title)} | #{ENV['COOP_SHORT_NAME']}"
-  	else
-  		"#{ENV['COOP_NAME']}"
-  	end
-  end
+# for page titles and meta descriptions
 
   def title(title)
   	content_for(:title, title)
   end
 
-	def page_description
-  	if content_for?(:description)
-  		"#{content_for(:description)}"
+	def page_title
+  	if content_for?(:title)
+  		"#{content_for(:title)} | #{ENV['COOP_SHORT_NAME']}"
   	else
   		"#{ENV['COOP_NAME']}"
   	end
@@ -24,21 +17,34 @@ module ApplicationHelper
   	content_for(:description, description)
   end
 
-	def resident_helper(person)
+	def page_description
+  	if content_for?(:description)
+  		"#{content_for(:description)}"
+  	else
+  		"#{ENV['COOP_DESCRIPTION']}"
+  	end
+  end
+
+# for people
+
+	def full_name(person)
 			name = person.first_name 
 			name << " " << person.last_name
 	end
 
-	def house_helper(property)
+# for one line address
+	def home_address(property)
 		address = property.house_number
 		address << " " << property.address1 << property.postcode
 	end
 
+# set currency
 	def show_money(money)
 		number_to_currency(money, unit: "£")
 	end
 
-	def day_helper(t)
+# date formats
+	def nice_date(t)
 		t.strftime("%A the #{t.day.ordinalize} %B %Y")	
 	end
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
+# used for text in footer
 ENV['COOP_FULL_NAME']   = 'Fictional Housing Cooperative Ltd'
-ENV['COOP_NAME']				=	'Fictional Housing Coop'
-ENV['COOP_SHORT_NAME']	=	'Fictional'
 ENV['COOP_FOOTER']    	= 'A fully mutual cooperative registered with the financial services authority under the industrial & provident societies act, 1965. Registration number: XXXXXX'
 ENV['COOP_ADDRESS']			=	'Fictional House, Fiction Street, ZZ1 1ZZ'
+# defaults for page title and meta description if not set with title and description helpers in view
+ENV['COOP_NAME']				=	'Fictional Housing Coop'
+ENV['COOP_SHORT_NAME']	=	'Fictional'
+ENV['COOP_DESCRIPTION']	=	'Fictional is a self-managing housing coop based in Fiction Town'
 
 # Load heroku vars from local file
 heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
