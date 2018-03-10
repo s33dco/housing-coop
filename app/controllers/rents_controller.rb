064e3561a3cfc45d6f528457d9edb11d8e438895
s33dco <code@s33d.co>
2018-03-10 22:55:21 +0000

    tidy view code around rent report

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -3,7 +3,7 @@ class RentsController < ApplicationController
 	def index
 		@payments = Rent.last_first
 		@money = @payments.total
-		@first = @payments.last.date
+		@first = @payments.last
 	end
 
 	def show
